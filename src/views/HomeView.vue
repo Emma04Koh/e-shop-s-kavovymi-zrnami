bdb5c97e2bc730b87125efbd91443fbce9d4aa3e
Emma04Koh <emmkak2004@gmail.com>
2025-12-17 20:02:39 +0100

    data

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+import CoffesData from '@/assets/data.json'
+
+export default defineComponent({
+  name: 'HomeView',
+  computed: {},
+})
+</script>
+
+<template></template>
