84d3ef29a50246bad175dbd3fe9c36027c355afd
Emma04Koh <emmkak2004@gmail.com>
2025-12-23 20:14:14 +0100

    HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,10 +2,31 @@
 import { defineComponent } from 'vue'
 import CoffeeData from '@/assets/data.json'
 
+export type CoffeeBean = {
+  name: string
+  slug: string
+  image: string
+  price: number
+  stock: number
+  taste_notes: string
+  description: string
+  grind_options: string[]
+}
+
+export type Coffee = {
+  name: string
+  slug: string
+  image: string
+  id: number
+  description: string
+  region_info: string
+  beans: CoffeeBean[]
+}
+
 export default defineComponent({
   name: 'HomeView',
   computed: {
-    coffee() {
+    coffeeD(): Coffee[] {
       return CoffeeData.coffee
     },
   },
@@ -17,12 +38,12 @@ export default defineComponent({
     <div class="home">
       <h1>Kávové zrná k Vaším službám</h1>
       <div class="coffee">
-        <div v-for="bean in coffee" :key="bean.id" class="coffee-item">
-          <RouterLink :to="{ name: 'bean', params: { BeanSlug: bean.slug } }">
-            <img :src="`/images/${bean.image}`" :alt="bean.name" />
+        <div v-for="coffee in coffeeD" :key="coffee.id" class="coffee-item">
+          <RouterLink :to="{ name: 'coffee', params: { CoffeeSlug: coffee.slug } }">
+            <img :src="`/images/${coffee.image}`" :alt="coffee.name" />
           </RouterLink>
-          <h2>{{ bean.name }}</h2>
-          <p>{{ bean.region_info }}</p>
+          <h2>{{ coffee.name }}</h2>
+          <p>{{ coffee.region_info }}</p>
         </div>
       </div>
     </div>
