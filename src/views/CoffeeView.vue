b9c31819da3802e709fdf5478a6241fac3de1b52
Emma04Koh <emmkak2004@gmail.com>
2025-12-23 20:20:29 +0100

    CoffeeView

---
+++ src/views/CoffeeView.vue
@@ -0,0 +1,37 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+import CoffeeData from '@/assets/data.json'
+import type { Coffee } from './HomeView.vue'
+
+export default defineComponent({
+  name: 'CoffeeView',
+  props: {
+    CoffeeSlug: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    coffee() {
+      const coffee = CoffeeData.coffee as Coffee[]
+      return coffee.find((c) => c.slug === this.CoffeeSlug)
+    },
+  },
+})
+</script>
+
+<template>
+  <button @click="$router.push('/')">NASPÄŤ</button>
+
+  <div class="coffee-details">
+    <div class="coffee-text">
+      <h1>{{ coffee?.name }}</h1>
+
+      <p><strong>Región:</strong> {{ coffee?.region_info }}</p>
+      <p>{{ coffee?.description }}</p>
+    </div>
+    <img :src="`/images/${coffee?.image}`" :alt="coffee?.name" />
+  </div>
+
+  <RouterView />
+</template>
