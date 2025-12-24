c2f3c39c4fbe60f6ce3dd521e94c714536c9afe2
Emma04Koh <emmkak2004@gmail.com>
2025-12-24 14:24:03 +0100

    router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import CoffeeView from '@/views/CoffeeView.vue'
+import BeansView from '@/views/BeansView.vue'
+import BeanDetail from '@/components/BeanDetail.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +17,21 @@ const router = createRouter({
       name: 'coffee',
       component: CoffeeView,
       props: true,
+      redirect: { name: 'beans' },
+      children: [
+        {
+          path: '',
+          name: 'beans',
+          component: BeansView,
+          props: true,
+        },
+        {
+          path: ':BeanSlug',
+          name: 'bean',
+          component: BeanDetail,
+          props: true,
+        },
+      ],
     },
   ],
 })
