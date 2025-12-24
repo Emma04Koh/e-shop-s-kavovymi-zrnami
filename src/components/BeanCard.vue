ccedf3f1a1c25e49dcc96e92b918f7e8d0532aba
Emma04Koh <emmkak2004@gmail.com>
2025-12-24 14:27:28 +0100

    BeanCard

---
+++ src/components/BeanCard.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'BeanCard',
+  props: {
+    bean: {
+      type: Object,
+      required: true,
+    },
+  },
+})
+</script>
+
+<template>
+  <div class="card">
+    <img :src="`/images/${bean.image}`" :alt="bean.name" />
+    <div class="card_text">{{ bean.name }}</div>
+  </div>
+</template>
