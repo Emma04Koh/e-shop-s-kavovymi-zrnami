0ad25bbc598918d6ee4c5490410f274d51970336
Emma04Koh <emmkak2004@gmail.com>
2026-01-19 18:52:45 +0100

    NotFoundView

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'NotFoundView',
+})
+</script>
+
+<template>
+  <div class="not-found">
+    <h1>404</h1>
+    <h2>Stránka nebola nájdená</h2>
+    <p>Ospravedlňujeme sa, ale stránka, ktorú hľadáte, tu nie je.</p>
+    <button @click="$router.push('/')">Späť na úvodnú stránku</button>
+  </div>
+</template>
